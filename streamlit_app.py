1a4a4de551e409c446acce8c221557de6e7846af
Ahmmmyoo <93283714+Ahmmmyoo@users.noreply.github.com>
2024-11-30 10:18:40 +0000

    added st.writes

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,18 @@ st.write(
 
 # Made a slider
 number = st.slider("Pick a Number", 0, 100, 50)
+st.write(
+    "Your selected number is ", number
+)
 
 pets = ["Dog", "Cat", "Bird"]
 pet = st.radio("Pick a pet", pets)
+st.write(
+    "Your selected pet is ", pet
+)
 
 date = st.date_input("Pick a date")
+st.write(
+    "Your selected date is ", date
+)
 
